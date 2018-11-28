5a98b90dabc539da6cf8902d42d23593e946ef36
Arif <44020819+azimarif@users.noreply.github.com>
2018-11-28 11:09:28 +0530

    Add isNeighbourAlive

--- src/library.js
+++ src/library.js
@@ -4,13 +4,17 @@ const world = {
   getWorldSize : function(grid){
     return grid.length;
   },
-  
+
   isNeighbourValid : function(position) {
     let { latitude, longitude } = position;
     return !( Math.min(latitude,longitude) < 0 || Math.max(latitude,longitude) >= this.getWorldSize(this.grid));
-  }
-}
+  },
 
+  isNeighbourAlive : function(position) {
+    let { latitude, longitude } = position;
+    return (this.isNeighbourValid(latitude,longitude)) &&  (this.grid[latitude][longitude] == 'A');
+  },
+}
 
 module.exports = { 
   world 
--- test/libraryTest.js
+++ test/libraryTest.js
@@ -1,9 +1,9 @@
 const { equal, deepEqual } = require('assert');
 let {  world } = require('../src/library.js');
 
-describe("test for isNeighbourValid ()",function() {
-  world.grid = [['A','D','A'], ['A','D','A'],['A','D','A']];
+world.grid = [['A','D','A'], ['A','D','A'],['A','D','A']];
 
+describe("test for isNeighbourValid ()",function() {
   it("should return false for negative neighbour positions",function() {
     equal(world.isNeighbourValid({latitude :0, longitude : 0}),true);
     equal(world.isNeighbourValid({latitude :-1, longitude : 0}),false);
@@ -20,4 +20,13 @@ describe("test for isNeighbourValid ()",function() {
   it('should return true for valid neighbour positions', function(){  
     equal(world.isNeighbourValid({latitude :2, longitude : 2}),true);
   })
+});
+
+describe("test for isNeighbourAlive ()",function() {
+  it("should return neighbour state",function() {
+    equal(world.isNeighbourAlive({latitude :0, longitude : 0}),true);
+    equal(world.isNeighbourAlive({latitude :1, longitude : 0}),true);
+    equal(world.isNeighbourAlive({latitude :0, longitude : 1}),false);
+    equal(world.isNeighbourAlive({latitude :1, longitude : 1}),false);
+  });
 });
